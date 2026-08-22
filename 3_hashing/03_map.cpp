8e302584c28243c12921a3c2477ed1aa6d16faa7
Rishabh <gangwarrishab94@gmail.com>
2026-08-23 01:06:56 +0530

    frequency of highest and lowest element

---
+++ 3_hashing/03_map.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Pre-compute
+    map<int, int> mpp;
+
+    for (int i = 0; i < n; i++) {
+        mpp[arr[i]]++;
+    }
+
+    // Queries
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int number;
+        cin >> number;
+
+        cout << mpp[number] << endl;
+    }
+
+    return 0;
+}
