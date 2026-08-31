48e912e753e6280d2c243b82e7ed8eb77ddfdbb7
Rishabh <gangwarrishab94@gmail.com>
2026-08-31 10:09:19 +0530

    linked list - static

---
+++ 2_arrays/class/linked list/static_linked_list.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+int main(){
+    struct Node n1,n2,n3;
+    n1.data = 10;
+    n2.data = 20;
+    n3.data = 30;
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = NULL;
+    return 0;
+
+}
